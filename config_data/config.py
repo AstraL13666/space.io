64b861d0bccca398e6c5e0d6a59f28800824aeee
Владимир Котов <vlad13kotov@gmail.com>
2023-09-06 18:35:06 +0300

    refactoring

--- config_data/config.py
+++ config_data/config.py
@@ -4,15 +4,14 @@ from dotenv import load_dotenv, find_dotenv
 
 from utils import Error
 
+connect = find_dotenv(".env")
+
 # Если файл .env не найден -> выходим из программы с выводом ошибки
-if not find_dotenv():
+if not connect:
     exit(Error(100))
 
-# иначе подгружаем значения
-else:
-    load_dotenv()
+else:  # иначе подгружаем значения
+    load_dotenv(connect)
+    t = getenv("token")
 
-token = getenv("token")
-admin = getenv("admin_id")
-logger_bot = getenv("logger_bot")
-rapid_api_key = getenv("rapid_api")
+    data = dict(token=getenv("token"), admin_id=getenv("admin_id"), log_bot=getenv("log_bot"))
