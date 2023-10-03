70c89e8ff28f19b3ca38542b35989e55a5218663
Vladimir Prin <69980366+AstraL13666@users.noreply.github.com>
2023-10-03 12:30:44 +0300

    Delete but.js

--- but.js
+++
@@ -1,8 +0,0 @@
-function loadReadme(filename) {
-  fetch(filename)
-    .then(response => response.text())
-    .then(text => {
-      const container = document.getElementById('readme-container');
-      container.innerHTML = text;
-    });
-}
